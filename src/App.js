bf23c5a5d746fade90d2affd892aafe90f53ad75
Archakov Dennis <amonbower@gmail.com>
2022-06-14 22:01:47 +0300

    init photos

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
+import React from 'react';
 import './index.scss';
 
-function Result() {
+function Collection({ name, images }) {
   return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
-    </div>
-  );
-}
-
-function Game() {
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+    <div className="collection">
+      <img className="collection__big" src={images[0]} alt="Item" />
+      <div className="collection__bottom">
+        <img className="collection__mini" src={images[1]} alt="Item" />
+        <img className="collection__mini" src={images[2]} alt="Item" />
+        <img className="collection__mini" src={images[3]} alt="Item" />
       </div>
-      <h1>Что такое useState?</h1>
-      <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
-      </ul>
-    </>
+      <h4>{name}</h4>
+    </div>
   );
 }
 
 function App() {
   return (
     <div className="App">
-      <Result />
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          <li className="active">Все</li>
+          <li>Горы</li>
+          <li>Море</li>
+          <li>Архитектура</li>
+          <li>Города</li>
+        </ul>
+        <input className="search-input" placeholder="Поиск по названию" />
+      </div>
+      <div className="content">
+        <Collection
+          name="Путешествие по миру"
+          images={[
+            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+          ]}
+        />
+      </div>
+      <ul className="pagination">
+        <li>1</li>
+        <li className="active">2</li>
+        <li>3</li>
+      </ul>
     </div>
   );
 }
