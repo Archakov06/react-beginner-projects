48d672f4031cc79bad5c1c39440f9ec4af47ad22
Archakov Dennis <amonbower@gmail.com>
2022-06-14 20:49:10 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>0</h1>
+        <button className="minus">- Минус</button>
+        <button className="plus">Плюс +</button>
+      </div>
     </div>
   );
 }
