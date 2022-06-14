cc3bb5a2c57eb0fa8ab12d14e03ea04f848e3ee8
Archakov Dennis <amonbower@gmail.com>
2022-06-14 21:23:02 +0300

    quiz init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './index.scss';
 
+function Result() {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали 3 ответа из 10</h2>
+      <button>Попробовать снова</button>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: '50%' }} className="progress__inner"></div>
+      </div>
+      <h1>Что такое useState?</h1>
+      <ul>
+        <li>Это функция для хранения данных компонента</li>
+        <li>Это глобальный стейт</li>
+        <li>Это когда на ты никому не нужен</li>
+      </ul>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
+      <Result />
     </div>
   );
 }
