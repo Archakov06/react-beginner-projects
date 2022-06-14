c51e11d14efc67331ec48dc375777c6360d380a0
Archakov Dennis <amonbower@gmail.com>
2022-06-14 21:23:02 +0300

    quiz init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import './index.scss';
 
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
+
+function Result() {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали 3 ответа из 10</h2>
+      <button>Попробовать снова</button>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: '50%' }} className="progress__inner"></div>
+      </div>
+      <h1>Что такое useState?</h1>
+      <ul>
+        <li>Это функция для хранения данных компонента</li>
+        <li>Это глобальный стейт</li>
+        <li>Это когда на ты никому не нужен</li>
+      </ul>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
+      <Game />
+      {/* <Result /> */}
     </div>
   );
 }
