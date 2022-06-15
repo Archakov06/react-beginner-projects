2bf0cdef72ba24f9e731516034053d45555f8e03
Archakov Dennis <amonbower@gmail.com>
2022-06-15 22:32:17 +0300

    init users

--- src/App.js
+++ src/App.js
@@ -1,69 +1,34 @@
+import React from 'react';
 import './index.scss';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
   return (
     <div className="App">
-      <div className="top">
-        <ul className="tabs">
-          <li className="active">Все</li>
-          <li>Открытые</li>
-          <li>Завершённые</li>
-        </ul>
-        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M15 2H9c-1.103 0-2 .897-2 2v2H3v2h2v12c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V8h2V6h-4V4c0-1.103-.897-2-2-2zM9 4h6v2H9V4zm8 16H7V8h10v12z" />
+      <div className="search">
+        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
+        <input type="text" placeholder="Найти пользователя..." />
       </div>
-      <div className="item">
-        <input class="styled-checkbox" id="checkbox2" type="checkbox" value="value1" />
-        <label for="checkbox2" />
-        <div className="item__content">
-          <b>Это еще одна задача</b>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ducimus
-            reprehenderit suscipit.
-          </p>
-          <div className="item__bottom">
-            <svg
-              enable-background="new 0 0 70 70"
-              height="70px"
-              id="Icons"
-              version="1.1"
-              viewBox="0 0 70 70"
-              width="70px">
-              <g>
-                <path d="M51,19h-4v-4h-4v4H27v-4h-4v4h-4c-2.209,0-4,1.791-4,4v28c0,2.209,1.791,4,4,4h32c2.209,0,4-1.791,4-4V23   C55,20.791,53.209,19,51,19z M51,51H19V31h32V51z M51,27H19v-4h32V27z" />
-                <rect height="4" width="4" x="35" y="35" />
-                <rect height="4" width="4" x="43" y="35" />
-                <rect height="4" width="4" x="35" y="43" />
-                <rect height="4" width="4" x="27" y="43" />
-              </g>
-            </svg>
-            15 июня 2022 год.
+      <ul className="users-list">
+        <li>
+          <div>
+            <img className="avatar" src="https://reqres.in/img/faces/1-image.jpg" alt="User" />
+            <div>
+              <h3>Amon Bower</h3>
+              <p>
+                <svg viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M48,0a48,48,0,0,0,0,96,6,6,0,0,0,0-12A36,36,0,1,1,84,48V66a6,6,0,0,1-12,0V48A24,24,0,1,0,48,72a23.7365,23.7365,0,0,0,12.2549-3.4783A17.9586,17.9586,0,0,0,96,66V48A48.0474,48.0474,0,0,0,48,0Zm0,60A12,12,0,1,1,60,48,12.0081,12.0081,0,0,1,48,60Z" />
+                </svg>
+                george.bluth@reqres.in
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="form">
-        <div className="form__checkbox">
-          <input class="styled-checkbox" id="add-checkbox" type="checkbox" value="value1" />
-          <label for="add-checkbox" />
-        </div>
-        <div class="form__fields">
-          <input type="text" placeholder="Название" className="input-title" />
-          <input type="text" placeholder="Введите текст..." className="input-text" />
-        </div>
-        <svg height="32px" viewBox="0 0 512 512" width="32px">
-          <g>
-            <g>
-              <g>
-                <path d="M256,48C141.1,48,48,141.1,48,256s93.1,208,208,208c114.9,0,208-93.1,208-208S370.9,48,256,48z M256,446.7     c-105.1,0-190.7-85.5-190.7-190.7S150.9,65.3,256,65.3S446.7,150.9,446.7,256S361.1,446.7,256,446.7z" />
-              </g>
-            </g>
-            <g>
-              <polygon points="264.1,128 247.3,128 247.3,247.9 128,247.9 128,264.7 247.3,264.7 247.3,384 264.1,384 264.1,264.7 384,264.7     384,247.9 264.1,247.9   " />
-            </g>
-          </g>
-        </svg>
-      </div>
+          <img className="action" src="/assets/plus.svg" alt="Action" />
+        </li>
+      </ul>
     </div>
   );
 }
