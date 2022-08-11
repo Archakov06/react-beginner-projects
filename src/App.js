441834fdaa1f075931d826dc7a96383dabadc91d
Archakov Dennis <amonbower@gmail.com>
2022-08-11 21:54:03 +0300

    init modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React from 'react';
 import './index.scss';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
+      <button className="open-modal-btn">✨ Открыть окно</button>
+      {/* <div className="overlay">
+        <div className="modal">
+          <svg height="200" viewBox="0 0 200 200" width="200">
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        </div>
+      </div> */}
     </div>
   );
 }
