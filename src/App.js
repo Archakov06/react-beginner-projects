b2a9333a74ef06e8cb7f0a081bac25448e19d7bc
Serhii <s.basanko@gmail.com>
2022-09-13 18:31:24 +0300

    Modal

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './index.scss';
 
 function App() {
+  const [open, setOpen] = React.useState(false);
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
+      <button  onClick={()=> setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+   { open && (
+    <div className={`overlay animated ${open ? 'show' : ''}`}>
         <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
+          <svg onClick={()=> setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         </div>
-      </div> */}
+      </div>
+      )}
     </div>
   );
 }
