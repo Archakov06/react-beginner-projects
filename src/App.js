07e57577c862e4e8917e6ee908c5c1a927fcb936
Anatoli Rak <Rack.anatoly@gmail.com>
2022-09-23 09:02:48 +0200

    calculator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [rates, setRates]=useState({});
+  
   return (
     <div className="App">
       <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
