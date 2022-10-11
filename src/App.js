0fdcf3a25cee0e1ad4f791cc2915e0264a592b74
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-10-11 14:39:36 +0200

    Modal Windows

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import './index.scss';
 
+
+
+
 function App() {
     const [open, setOpen] = useState(false) // by default window is close
 
@@ -8,21 +11,39 @@ function App() {
     return (
         <div className="App">
             <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Open Window</button>
-           {/*1 Conditional render*/}
-           {/* If open state is true, window will be open and if open state false, window will close */}
-            {open && (
-                <div className="overlay">
-                    <div className="modal">
-                        <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-                            <title/>
-                            <path
-                                d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
-                        </svg>
-                        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>
-                    </div>
-                </div>
-            )
-            }
+                       {/*1 Conditional render*/}
+
+            {/* If open state is true, window will be open and if open state false, window will close */}
+            {/* Minus of conditional render that we cannot make animation, window will be quick open and quick close */}
+            {/*{open && (*/}
+            {/*    <div className="overlay">*/}
+            {/*        <div className="modal">*/}
+            {/*            <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">*/}
+            {/*                <title/>*/}
+            {/*                <path*/}
+            {/*                    d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>*/}
+            {/*            </svg>*/}
+            {/*            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*   )*/}
+            {/*}*/}
+
+
+                      {/* 2 Make an animated modal window*/}
+                {/* To animate modal, using css classes*/}
+                {/*Add conditions*/}
+                {/*<div className={`overlay animated ${open ? 'show' : ''}`}>*/}
+                {/*    <div className="modal">*/}
+                {/*        <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">*/}
+                {/*            <title/>*/}
+                {/*            <path*/}
+                {/*                d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>*/}
+                {/*        </svg>*/}
+                {/*        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+
         </div>
     );
 }
