5c04c8e70fcc4faae3b02ad855a09e69a1088978
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-10-12 00:17:28 +0300

    feat: take data from backened, fake loading

--- src/App.js
+++ src/App.js
@@ -6,12 +6,40 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+    // 1. Запрашиваем с бека данные
+    const [users, setUsers] = React.useState([])
+
+    // При загрузки страницы у нас уже должен отправляться запрос
+    const [isLoading, setLoading] = React.useState(true)
+
+    // 2. При первом рендеринге мы будем отправлять запрос на бекенд и для этого
+    // мы используем функцию fetch
+    React.useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            // 3.  Преобразуем файл в json
+            .then(res => res.json())
+            // 4. Из всего json нам необходимо вытащить data и получить массив
+            .then(json => {
+                setUsers(json.data)
+            })
+            // 5. Если вдруг произошла ошибка, то мы отлавливаем его с помощью
+            // catch
+            .catch((err) => {
+                console.warn(err)
+                alert("Ошибка при получении пользователей")
+            })
+            // 9. Финальная часть, она будет менять loading на false
+            // говорить о том, что загрузка завершилась
+            .finally(() => setLoading(false))
+    },[]);
+
+
+    return (
+        <div className="App">
+            <Users items={users} isLoading={isLoading}/>
+            {/* <Success /> */}
+        </div>
+    );
 }
 
 export default App;
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
-import { Skeleton } from './Skeleton';
-import { User } from './User';
+import {Skeleton} from './Skeleton';
+import {User} from "./User";
 
-export const Users = ({ items, isLoading }) => {
-  return (
-    <>
-      <div className="search">
-        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
-        </svg>
-        <input type="text" placeholder="Найти пользователя..." />
-      </div>
-      {isLoading ? (
-        <div className="skeleton-list">
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-        </div>
-      ) : (
-        <ul className="users-list">
-          <User />
-        </ul>
-      )}
-      <button className="send-invite-btn">Отправить приглашение</button>
-    </>
-  );
+export const Users = ({items, isLoading}) => {
+    // 6. isLoading - позволяет нам отрендарить скелетон, другими словами - это
+    // ложная загрузка (3 блоков) означает, что сейчас что-то появится
+    return (
+        <>
+            <div className="search">
+                <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
+                </svg>
+                <input type="text" placeholder="Найти пользователя..."/>
+            </div>
+
+            {/* 7. Работает лживая загрузка вот здесь, если оно true, а если
+            оно false, то рендерится лишь один блок, то есть начальная страница
+            пользователей */}
+            {isLoading ? (
+                <div className="skeleton-list">
+                    <Skeleton/>
+                    <Skeleton/>
+                    <Skeleton/>
+                </div>
+            ) : (
+                <ul className="users-list">
+                    {/* 8. Тут мы берем (получаем) данные items, из него вытаскиваем каждого пользователя и передаем внутрь*/}
+                    {items.map((obj) => (
+                        /* 13. Если у нас в объекте хранятся такие же
+                        объекты, что и пропсы
+                           first_name = {obj.first_name}
+                             last_name = {obj.last_name}
+                              email = {obj.email}
+                               avatar = {obj.avatar}
+                            можно сократить и передать
+                         */
+                        // Также необходимо передать ключ, так как мы рендерим его с помощью map
+                        <User key={obj.id} {...obj} />
+                    ))}
+                </ul>
+            )}
+            <button className="send-invite-btn">Отправить приглашение</button>
+        </>
+    );
 };
