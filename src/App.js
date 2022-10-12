d24173a1d1c3a881dddb59f7e2d735581b2c8cc2
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-10-12 17:04:48 +0300

    feat: request from Backened for valuts, and choose buttons valut

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+    // 1. Стейт для валюты (по умолчанию будет пустой объект, в которой
+    // потом запихнем данные из бека
+    const [rates, setRates] = React.useState({})
+
+    // 3. Будущий функционал для выбора валюты (От (первая колонка) - до (вторая колонка) ).
+    // По умолчанию будет выбран рубль
+    const [fromCurrency, setFromCurrency] = React.useState("RUB")
+    const [toCurrency, setToCurrency] = React.useState("USD")
+
+    // 2. Запрос на бекенд
+    React.useEffect(() => {
+        fetch('https://cdn.cur.su/api/latest.json')
+            .then((res) => res.json())
+            .then((json) => {
+                setRates(json.rates);
+                console.log(json.rates)
+            })
+            .catch((err) => {
+                console.warn(err);
+                alert("Не уделаось получить инфу")
+            })
+        }, [])
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+        <Block value={0}
+               currency={fromCurrency}
+               onChangeCurrency={setFromCurrency}
+        />
+        <Block value={0}
+               currency={toCurrency}
+               onChangeCurrency={setToCurrency}
+        />
     </div>
   );
 }
