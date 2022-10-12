183478a5c4be1c01ed9f21ca1def531fb33df416
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-10-12 22:23:06 +0300

    choose valuts and non-syncron-code

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
-    const [rates, setRates] = React.useState({})
+    // const [rates, setRates] = React.useState({})
+
+    /* Хук useRef позволяет сохранить некоторый объект, который можно можно изменять и который хранится в течение всей жизни компонента. В качестве параметра функция useRef() принимает начальное значение хранимого объекта. А возвращаемое значение - ссылка-объект, из свойства current которого можно получить хранимое значение.
+     */
+    const ratesRef = React.useRef({})
+
     const [fromCurrency, setFromCurrency] = React.useState("RUB")
     const [toCurrency, setToCurrency] = React.useState("USD")
     const [fromPrice, setFromPrice] = React.useState(0)
-    const [toPrice, setToPrice] = React.useState(0)
+    const [toPrice, setToPrice] = React.useState(1)
 
     React.useEffect(() => {
         fetch('https://cdn.cur.su/api/latest.json')
             .then((res) => res.json())
             .then((json) => {
-                setRates(json.rates);
-                console.log(json.rates)
+                ratesRef.current = json.rates
+                onChangeToPrice(1)
             })
             .catch((err) => {
                 console.warn(err);
@@ -23,23 +28,41 @@ function App() {
         }, [])
 
     const onChangeFromPrice = (value) => {
-        const price = value / rates[fromCurrency]
-        const result = price * rates[toCurrency]
+        const price = value /  ratesRef.current[fromCurrency]
+        const result = price *  ratesRef.current[toCurrency]
+        setToPrice(result.toFixed(3))
         setFromPrice(value)
-        setToPrice(result)
     }
-
     const onChangeToPrice = (value) => {
-        const result = (rates[fromCurrency] / rates[toCurrency]) * value
-        setFromPrice(result);
+        const result = ( ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
+        setFromPrice(result.toFixed(3));
         setToPrice(value)
     }
+
+    // Функция для изменения валюту
+    const onChangeFromCurrency = (cur) => {
+        // Сохраняем саму валюту
+        setFromCurrency(cur)
+        // И паралельно сумму от той валюты, которую мы изменили
+        // мы будем вносить во вторую колонку
+        onChangeFromPrice(fromPrice)
+    }
+
+    React.useEffect(() => {
+        onChangeFromPrice(fromPrice)
+        // 1. Дождись когда наш каренси изменится и только после этого ты должен взять часть
+    }, [fromCurrency])
+
+    React.useEffect(() => {
+        onChangeToPrice(toPrice)
+        // 1. Дождись когда наш каренси изменится и только после этого ты должен взять часть
+    }, [toCurrency])
 
   return (
     <div className="App">
         <Block value={fromPrice}
                currency={fromCurrency}
-               onChangeCurrency={setFromCurrency}
+               onChangeCurrency={onChangeFromCurrency}
                onChangeValue={onChangeFromPrice}
         />
 
