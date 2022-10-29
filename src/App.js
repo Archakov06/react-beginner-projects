84fde827fdc79d85f646894d3c94952df6974a2c
PKG-7 <pavel.k.group@gmail.com>
2022-10-29 20:39:52 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
-
+//
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
