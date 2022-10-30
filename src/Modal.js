7dac4d91cd05254b968c63a7f4e9f36db339b53a
Egor Tarnaev <egortarnaev@mail.ru>
2022-10-30 19:26:30 +0300

    modal done

---
+++ src/Modal.js
@@ -0,0 +1,13 @@
+export default function Modal(props){
+    return (
+      <div className={`overlay animated ${props.isTrue ? 'show' : ''}`}>
+          <div className="modal">
+            <svg height="200" viewBox="0 0 200 200" width="200" onClick={props.closeBtn}>
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
+      </div>
+    )
+  }
