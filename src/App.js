880e359fc488559215b75c46986a6973e9cc58ee
PKG-7 <pavel.k.group@gmail.com>
2022-11-02 23:42:50 +0300

    not ready

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+import { useEffect } from 'react';
+import { useState } from 'react';
 //
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const link = 'https://reqres.in/api/users';
+  const [items, setItems] = useState(0);
+
+  useEffect(() => {
+    fetch(link)
+      .then(res => res.json())
+      .then(json => {
+        setItems(json.data);
+        console.log(json.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <Users />
+    <div className='App'>
+      <Users items={items} />
       {/* <Success /> */}
     </div>
   );
