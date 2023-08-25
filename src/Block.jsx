b59d6fb2f0b7e105213984c9c61b74b5520d9387
kvizhard777 <kvizhard.acc@gmail.com>
2023-08-25 12:40:42 +0500

    Error API

--- src/Block.jsx
+++ src/Block.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
+const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP']
 
 export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
   <div className="block">
@@ -27,4 +27,4 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
       placeholder={0}
     />
   </div>
-);
+)
