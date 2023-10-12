c04fa4f4992173e4fae9890c2578c23e76518b1d
De-Konstantin <denkakonstantinov@gmail.com>
2023-10-12 16:28:04 +0300

    Added 3 variants of modal window   (generated by useblackbox.ai )

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import './index.scss';
-
+import "./index.scss";
+import React, { useState } from "react";
 function App() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      <button onClick={() => setOpen(true)} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+      {/* условный рендеp */}
+      {/* {open && (
+        <div className="overlay">
+          <div className="modal">
+            <svg
+              onClick={() => setOpen(false)}
+              height="200"
+              viewBox="0 0 200 200"
+              width="200"
+            >
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
         </div>
-      </div> */}
+      )} */}
+
+      {/* {open && (
+        <div className="overlay">
+          <div className="modal">
+            <svg
+              onClick={() => setOpen(false)}
+              height="200"
+              viewBox="0 0 200 200"
+              width="200"
+            >
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
+        </div>
+      )} */}
     </div>
   );
 }
